4ba1e29faaa70656f8172ca5db7137a8cfb70232
Golsa <golsaeskandari76@gmail.com>
2023-09-01 14:36:13 +0330

    changing js

--- guessing-game/app.js
+++ guessing-game/app.js
@@ -1,12 +1,3 @@
-//We're gonna make a guessing game OK?
-//We make a random number with Random function from JS
-//Then we keep it in a variable 
-//A textbox and a button for the user to guess the random number
-//If the number user enter is lower than the random number we say go up
-//If the number user enter is greater than the random number we say go down
-//If the user guess the number right, less than 10 tries he wins
-//And above 10 tries he loses. OK? Got it?
-
 var mynumber = Math.floor((Math.random() * 100));
 var remainGuesse = 10;
 console.log('The number is ' + mynumber);
